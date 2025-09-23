c3e189d05b6118fae2e99cd7d55438e7d871edeb
LimdaeIl <optional94@naver.com>
2025-09-23 13:33:10 +0900

    ✨️#15: 스프링 부트 .env 파일 로드 지정
    스프링 부트가 읽어야 할 .env 파일과 도커 컴포즈가 읽어야 할 .env 파일을 구분해야 합니다. 따라서 .env 파일을 루트 디렉토리 기준으로 위치한 .env 파일을 시스템 프로퍼티에 등록합니다.

--- src/main/java/com/friday/commerce/CommerceApplication.java
+++ src/main/java/com/friday/commerce/CommerceApplication.java
@@ -1,5 +1,6 @@
 package com.friday.commerce;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommerceApplication.class, args);
+        // .env 파일 로드 (resources 기준 경로로)
+        Dotenv dotenv = Dotenv.configure()
+                .directory(".")
+                .ignoreIfMissing()
+                .load();
+
+        // 시스템 프로퍼티에 등록 (Spring 이 인식할 수 있도록)
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+
+        SpringApplication.run(CommerceApplication.class, args);
 	}
 
 }
