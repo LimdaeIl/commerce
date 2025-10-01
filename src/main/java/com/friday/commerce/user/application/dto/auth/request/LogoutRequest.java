22877aa718c2b1b0ad126ea5c9b974c7fb7757f5
LimdaeIl <optional94@naver.com>
2025-10-01 15:59:04 +0900

    ✨️#60: Validation 추가

--- src/main/java/com/friday/commerce/user/application/dto/auth/request/LogoutRequest.java
+++ src/main/java/com/friday/commerce/user/application/dto/auth/request/LogoutRequest.java
@@ -1,9 +1,9 @@
 package com.friday.commerce.user.application.dto.auth.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record LogoutRequest(
-        @NotNull
+        @NotBlank(message = "리프레시 토큰: 토큰은 필수입니다.")
         String rt
 ) {
 
