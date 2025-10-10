f2fd84b1d7787339243d7a3d6f346fed73acf478
LimdaeIl <optional94@naver.com>
2025-10-10 13:21:12 +0900

    ✨️#100: 스키마 올바르게 속성 사용하도록 수정
    name 이 아니라 description 으로 수정

--- src/main/java/com/friday/commerce/user/application/dto/auth/request/VerifyCodeEmailRequest.java
+++ src/main/java/com/friday/commerce/user/application/dto/auth/request/VerifyCodeEmailRequest.java
@@ -8,12 +8,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record VerifyCodeEmailRequest(
-        @Schema(name = "이메일", example = "alice@example.com")
+        @Schema(description = "이메일", example = "alice@example.com")
         @NotBlank(message = "이메일: 이메일은 필수입니다.")
         @Email(message = "이메일: 유효하지 않은 이메일 형식입니다.")
         String email,
 
-        @Schema(name = "코드", example = "220565")
+        @Schema(description = "코드", example = "220565")
         @NotNull
         @Min(100000)
         @Max(999999)
